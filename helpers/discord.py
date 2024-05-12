f285f51ee47a9d86896286972d41e03554efbe36
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2024-05-12 17:16:50 +0300

    Add prefix for dev mode

--- helpers/discord.py
+++ helpers/discord.py
@@ -11,11 +11,13 @@ async def send_message(message):
     TOKEN = os.environ.get('DISCORD_TOKEN')
     #logger.info('The TOKEN is')
     #logger.info(TOKEN)
-    
+
     # Retrieve the Discord channel ID from environment variables
     CHANNEL = os.environ.get('DISCORD_CHANNEL')
     #logger.info('The Channel is')
     #logger.info(CHANNEL)
+
+    PREFIX = os.environ.get('DISCORD_MESSAGE_PREFIX')
 
     # Define intents
     intents = discord.Intents.default()
@@ -26,16 +28,16 @@ async def send_message(message):
     @client.event
     async def on_ready():
         channel = client.get_channel(int(CHANNEL))
-        await channel.send(message)
+        await channel.send(PREFIX + message)
 
     await client.start(TOKEN)
-    
+
 def init_send_message(message):
-    
+
     from tasks import send_message_async
     try:
         result = send_message_async.delay(message)
         logger.info(f"Celery send_message_async task called successfully! Task ID: {result.id}")
     except Exception as e:
         logger.error("This is an error message from helpers/discord.py while trying to send_message_async")
-        logger.error(e)    
+        logger.error(e)
