8a4eaa8ce0814849b4fb9ea4a36381dd3da0ca17
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2024-09-21 14:21:51 +0300

    Check all pages of goodgrand users, not only the first one

--- helpers/goodgrands.py
+++ helpers/goodgrands.py
@@ -33,27 +33,24 @@ def get_applications():
 
 
 def get_goodgrands_users():
-    # Replace with your actual API key
     api_key = os.environ.get("GOODGRANDS_API_KEY")
     url = "https://api.cr4ce.com/user"
-
-    # Set up headers for the request
     headers = {
-        "Accept": "application/vnd.Creative Force.v2.1+json",  # JSON response
+        "Accept": "application/vnd.Creative Force.v2.1+json",
         "x-api-key": api_key,
         "x-api-language": "en_GB",
     }
+    all_users = []
+    while url:
+        response = requests.get(url, headers=headers)
 
-    # Make a GET request to the API
-    response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            all_users.extend(data["data"])  # Append the users to the list
+            url = data.get("next_page_url")  # Get the next page URL
+        else:
+            logger.info(f"Error: {response.status_code}")
+            logger.info(response.text)
+            break
 
-    # Check the response
-    if response.status_code == 200:
-        logger.info("Success!")
-        logger.info(response.json())  # Print the returned data
-        return response.json()
-    else:
-        logger.info(f"Error: {response.status_code}")
-        logger.info(response.text)
-
-    return None
+    return all_users if all_users else None
